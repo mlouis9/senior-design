75553299ba298d333095b200a1343d23b0ac3248
Matthew Louis <mlouis9@gatech.edu>
2024-03-13 12:57:23 -0400

    Finished thermophysical property benchmarking

--- saltOptimization/thermophysicalProperties/benchmarking/README.md
+++ saltOptimization/thermophysicalProperties/benchmarking/README.md
@@ -1,5 +1,5 @@
 # Thermophysical Property Calculation Benchmarking
-This directory shows the benchmarking of thermophysical property calcualtions using the custom python module: thermophysicalProperties.
+This directory shows the benchmarking of thermophysical property calcualtions using the custom python module: [thermophysicalProperties](../../../modules/thermophysicalProperties.py).
 
 ## Test Cases
 
@@ -8,9 +8,23 @@ This directory shows the benchmarking of thermophysical property calcualtions us
 2. Calculating the density of the ternary KCl-NaCl-UCl3 system along the slice NaCl : UCl3 = 0.25 : 0.75, again the results are compared to corresponding plots from [[1]](#agca-empirical)
 
 ### Fluoride RK Comparisons
-1. Calculating the density of the ternary NaF-LiF-ZrF4 system along the slice <br> NaF : LiF = 0.4 : 0.6. These results are compared to plots from [[2]](#birri-application)
+1. Calculating the density of the ternary NaF-LiF-ZrF4 system along the slice NaF : LiF = 0.4 : 0.6. These results are compared to plots from [[2]](#birri-application)
+
+### Heat Capacity
+1. The heat capacity of the following pseudo binary systems was calculated and compared to the data given in [[3]](#beilmann-excess):
+   1. LiF-KF
+   2. LiF-NaF
+
+### Thermal Conductivity
+1. The thermal conductivity of the KCl-MgCl$_2$ system was calculated, and compared with the corresponding plot in [[4]](#xu-experimental)
+
+### Viscosity
+1. The dynamic viscosity of LiF-NaF-KF (FLiNaK), LiF-BeF2 (66-34 mol%), and LiF-BeF2 (73-27 mol%) were calculated and compared to the reference [[5]](#tkacheva-viscosity)
 
 ## References
 
 1. <a name="agca-empirical"></a> [Can Agca, "Empirical estimation of densities in NaCl-KCl-UCl3 and NaCl-KCl-YCl3 molten salts using Redlich-Kister expansion", Chemical Engineering Science](https://www.sciencedirect.com/science/article/pii/S0009250921006515?via%3Dihub)
 2. <a name="birri-application"></a> [Anthony Birri, "Application of the Redlich-Kister expansion for estimating the density of molten fluoride psuedo-ternary salt systems of nuclear industry interest", Chemical Engineering Science](https://www.sciencedirect.com/science/article/pii/S0009250922005383#f0010)
+3. <a name="beilmann-excess"></a> [M. Beilmann, "Excess Heat Capacity in Liquid Binary Alkali-Fluoride Mixtures", Inorg. Chem](https://pubs.acs.org/doi/full/10.1021/ic302168g)
+4. <a name="xu-experimental"></a> [Xiankun Xu, "Experimental Test of Properties of KCl–MgCl2 Eutectic Molten Salt for Heat Transfer and Thermal Storage Fluid in Concentrated Solar Power Systems", Journal of Solar Energy Engineering](https://asmedigitalcollection.asme.org/solarenergyengineering/article/140/5/051011/368304/Experimental-Test-of-Properties-of-KCl-MgCl2)
+5. <a name="tkacheva-viscosity"></a> [Olga Tkacheva, "Viscosity of fluoride melts promising for molten salt nuclear reactors", Electrochemical Materials and Technologies](https://www.researchgate.net/publication/375712728_Viscosity_of_fluoride_melts_promising_for_molten_salt_nuclear_reactors)
--- saltOptimization/thermophysicalProperties/benchmarking/generateBenchmark.py
+++ saltOptimization/thermophysicalProperties/benchmarking/generateBenchmark.py
@@ -2,6 +2,10 @@ from pathlib import Path
 from thermophysicalProperties import Database
 import matplotlib.pyplot as plt
 import numpy as np
+from frozendict import frozendict
+import pint
+ureg = pint.UnitRegistry(auto_reduce_dimensions=True)
+from uncertainties import ufloat
 
 script_dir = Path(__file__).resolve().parent
 
@@ -64,4 +68,88 @@ plt.ylabel('Density kg/m$^3$')
 plt.grid()
 plt.tight_layout()
 plt.savefig(str( script_dir / 'plots' / 'NaFLiFZrF4Calculated.png' ), dpi=500)
+plt.clf()
+
+# -------------------------
+# Heat Capacity Test Cases
+# -------------------------
+
+mole_frac_LiF = np.linspace(0,1.0,10)
+temp = 1200 # Assume this temp, not clearly stated in the ref
+other_endmembers = ['KF', 'NaF']
+experiemental_datapoints = {
+    'KF': [(0.75, ufloat(69.1, 4.6)), (0.5, ufloat(77.1, 5.8)), (0.25, ufloat(75, 5.5))], 
+    'NaF': [(0.8, ufloat(65, 5)), (0.6, ufloat(67, 5)), (0.4, ufloat(70, 6)), (0.2, ufloat(69, 6.5))]
+}
+
+for other_endmember in other_endmembers:
+    test_salts = [ {'LiF': x, other_endmember: (1-x)} for x in mole_frac_LiF]
+
+    # Need MW of each endmember to convert to heat capacity in a per mole basis
+    mw_LiF = db.data[frozendict({'LiF': 1.0})]['molecular_weight']
+    mw_other = db.data[frozendict({other_endmember: 1.0})]['molecular_weight']
+    mw_test_salts = [ (x*mw_LiF + (1-x)*mw_other)*ureg('g/mol').to('kg/mol').magnitude for x in mole_frac_LiF]
+
+    # Now get the heat capacities and uncertainties
+    heat_capacities = [ db.get_tp('liquid_heat_capacity', test_salt)(temp).nominal_value*mw_test_salt \
+                       for test_salt, mw_test_salt in zip(test_salts, mw_test_salts) ]
+    heat_capacities_unc = [ db.get_tp('liquid_heat_capacity', test_salt)(temp).std_dev*mw_test_salt \
+                            for test_salt, mw_test_salt in zip(test_salts, mw_test_salts) ]
+    plt.errorbar(mole_frac_LiF, heat_capacities, yerr=heat_capacities_unc, capsize=2.5, elinewidth=1.5, \
+                 label=f"LiF-{other_endmember}", markersize=6)
+    experimental_unc = [datapoint[1].std_dev for datapoint in experiemental_datapoints[other_endmember]]
+    experimental_value = [datapoint[1].nominal_value for datapoint in experiemental_datapoints[other_endmember]]
+    xvals = [datapoint[0] for datapoint in experiemental_datapoints[other_endmember]]
+    plt.errorbar(xvals, experimental_value, yerr=experimental_unc, fmt='.', capsize=2.5, elinewidth=1.5,\
+                 label=f"LiF-{other_endmember} Experimental", markersize=6)
+
+plt.legend(loc='upper left', bbox_to_anchor=(0.9,1))
+plt.xlabel('Mole Fraction LiF')
+plt.ylabel('Heat Capacity $\\frac{\\text{J}}{\\text{K}\\cdot \\text{mol}}$')
+plt.grid()
+plt.tight_layout()
+plt.savefig(str( script_dir / 'plots' / 'LiFCsFetcHeatCapacityCalculated.png' ), dpi=500)
+plt.clf()
+
+# --------------------------------
+# Thermal Conductivity Test Cases
+# --------------------------------
+
+# KCl-MgCl2 (0.68-0.32)
+test_salt = {'KCl': 0.68, 'MgCl2': 0.32}
+temps = np.linspace(450 + 273.15,850 + 273.15, 10)
+thermal_conductivities = [db.get_tp('thermal_conductivity', test_salt)(temp).nominal_value for temp in temps]
+thermal_conductivities_unc = [db.get_tp('thermal_conductivity', test_salt)(temp).std_dev for temp in temps]
+plt.errorbar(temps - 273.15, thermal_conductivities, yerr=thermal_conductivities_unc, capsize=2.5)
+# plt.legend(loc='upper left', bbox_to_anchor=(0.9,1))
+plt.xlabel('Temperature ($^\circ \\text{C}$)')
+plt.ylabel('Thermal Conductivity $\\frac{\\text{W}}{\\text{m}\\cdot \\text{K}}$')
+plt.grid()
+plt.tight_layout()
+plt.savefig(str( script_dir / 'plots' / 'MgCl2KClThermalConductivity.png' ), dpi=500)
+plt.clf()
+
+# ----------------------
+# Viscosity Test Cases
+# ----------------------
+
+test_salts = [ 
+    {'LiF': 1/3,'NaF': 1/3, 'KF': 1/3} ,
+    {'LiF': 0.66, 'BeF2': 0.34},
+    {'LiF': 0.73, 'BeF2': 0.27}
+]
+labels = ['FLiNaK', 'LiF-BeF2 (66-34 mol%)', 'LiF-BeF2 (73-27 mol%)']
+temps = np.linspace(600 + 273.15, 700 + 273.14, 10)
+for index, test_salt in enumerate(test_salts):
+    viscosity = [db.get_tp('viscosity', test_salt)(temp).nominal_value*1E+03 \
+                 for temp in temps]
+    viscosity_unc = [db.get_tp('viscosity', test_salt)(temp).std_dev*1E+03 \
+                     for temp in temps]
+    plt.errorbar(temps - 273.15, viscosity, yerr=viscosity_unc, capsize=2.5, elinewidth=1.5, label=labels[index])
+plt.legend(loc='upper left', bbox_to_anchor=(0.9,1))
+plt.xlabel('Temperature ($^\circ \\text{C}$)')
+plt.ylabel('Dynamic Viscosity $\\text{mPa}\cdot \\text{s}$')
+plt.grid()
+plt.tight_layout()
+plt.savefig(str( script_dir / 'plots' / 'FLiNaKViscosity.png' ), dpi=500)
 plt.clf()
