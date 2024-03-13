a67b4664a5b1c9758170550ae222cbce8e54a1ec
Matthew Louis <mlouis9@gatech.edu>
2024-03-12 20:57:19 -0400

    Finished RK density benchmarking, along with documentation. Still need to do benchmarking for other properties

--- saltOptimization/thermophysicalProperties/benchmarking/generateBenchmark.py
+++ saltOptimization/thermophysicalProperties/benchmarking/generateBenchmark.py
@@ -11,8 +11,10 @@ mstdb_tp_rk_path = Path('/home/mlouis9/mstdb-tp/Molten_Salt_Thermophysical_Prope
 db = Database(mstdb_tp_path, mstdb_tp_rk_path)
 
 # ----------------------
-# Test case 1: KCl-NaCl
+# Chloride Test Cases
 # ----------------------
+
+# Test case 1: KCl-NaCl
 
 mole_frac_NaCl = np.linspace(0,1,10)
 temps = [943, 1033, 1123, 1213, 1303]
@@ -26,3 +28,40 @@ plt.ylabel('Density kg/m$^3$')
 plt.grid()
 plt.tight_layout()
 plt.savefig(str( script_dir / 'plots' / 'KClNaClCalculated.png' ), dpi=500)
+plt.clf()
+
+# Test case 2: KCl-NaCl-UCl3
+
+mole_frac_KCl = np.linspace(0,0.9,10)
+temps = [900,985, 1070, 1155, 1240]
+for temp in temps:
+    test_salts = [ {'KCl': x, 'NaCl': (1-x)*0.25, 'UCl3': (1-x)*0.75} for x in mole_frac_KCl]
+    densities = [ db.get_tp('density', test_salt)(temp).nominal_value for test_salt in test_salts ]
+    plt.plot(mole_frac_KCl, densities, label=f"{temp} K")
+plt.legend(loc='upper left', bbox_to_anchor=(0.9,1))
+plt.xlabel('Mole Fraction KCl')
+plt.ylabel('Density kg/m$^3$')
+plt.grid()
+plt.tight_layout()
+plt.savefig(str( script_dir / 'plots' / 'KClNaClUCl3Calculated.png' ), dpi=500)
+plt.clf()
+
+# ----------------------
+# Fluoride Test Cases
+# ----------------------
+
+# Test case 1: NaF-LiF-ZrF4
+
+mole_frac_ZrF4 = np.linspace(0,1.0,10)
+temps = [900,985, 1070, 1155, 1240]
+for temp in temps:
+    test_salts = [ {'ZrF4': x, 'NaF': (1-x)*0.4, 'LiF': (1-x)*0.6} for x in mole_frac_ZrF4]
+    densities = [ db.get_tp('density', test_salt)(temp).nominal_value for test_salt in test_salts ]
+    plt.plot(mole_frac_ZrF4, densities, label=f"{temp} K")
+plt.legend(loc='upper left', bbox_to_anchor=(0.9,1))
+plt.xlabel('Mole Fraction ZrF$_4$')
+plt.ylabel('Density kg/m$^3$')
+plt.grid()
+plt.tight_layout()
+plt.savefig(str( script_dir / 'plots' / 'NaFLiFZrF4Calculated.png' ), dpi=500)
+plt.clf()
