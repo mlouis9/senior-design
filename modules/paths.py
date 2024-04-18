454a57d534ffe37af3786e4c8195a316ebc15173
Matthew Louis <mlouis9@gatech.edu>
2024-04-18 11:40:12 -0400

    Finished short fuel case, and wrote case for full length fuel simulation: wrote a new full length mesh, updated thermophysical properties, temperature profile, boundary conditions, and calculated polynomial fits from final properties

--- modules/paths.py
+++ modules/paths.py
@@ -79,3 +79,4 @@ SALT_OPTIMIZATION = here("saltOptimization")
 
 AVERAGE_COOLANT_CASE = here("cfd/3Dpincell/averageCoolant")
 SHORT_FUEL_CASE = here("cfd/3Dpincell/shortFuel")
+AVERAGE_FUEL_CASE = here("cfd/3Dpincell/averageFuel")
